3961e91ebeee6007e23cd1afdfde44688a825a7c
Chema <josem.perram@educa.jcyl.es>
2023-11-02 16:33:38 +0100

    Creada estructura de clases del proyecto de la biblioteca.

---
+++ src/Autor.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+public class Autor {
+    private String nombre;
+    private String apellidos;
+    private String fechaNacimiento;
+    private ArrayList<Libro> libros;
+
+    private String localidad;
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+
+    public ArrayList<Libro> getLibros() {
+        return libros;
+    }
+
+    public void setLibros(ArrayList<Libro> libros) {
+        this.libros = libros;
+    }
+}
